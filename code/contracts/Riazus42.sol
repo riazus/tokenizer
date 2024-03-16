2c32708972cd34ba3f95b77637cfb03475e177c8
riazus <riyaz@alexlegal.fr>
2024-03-16 11:39:04 +0100

    feat: new token Riazus42 and deploy script

---
+++ code/contracts/Riazus42.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract Riazus42 is ERC20, ERC20Burnable, Ownable, ERC20Permit {
+    constructor(address initialOwner)
+        ERC20("Riazus42", "RIA")
+        Ownable(initialOwner)
+        ERC20Permit("Riazus42")
+    {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
+
